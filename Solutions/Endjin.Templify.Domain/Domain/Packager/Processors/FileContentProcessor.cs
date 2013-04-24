9f70c0d9565cb28a5fc5f056a79fe7e6c0fbeac8
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2013-04-24 12:43:31 +0100

    Support reading and writing a file with the same file encoding.

--- Solutions/Endjin.Templify.Domain/Domain/Packager/Processors/FileContentProcessor.cs
+++ Solutions/Endjin.Templify.Domain/Domain/Packager/Processors/FileContentProcessor.cs
@@ -15,12 +15,16 @@ namespace Endjin.Templify.Domain.Domain.Packager.Processors
     {
         public string ReadContents(string path)
         {
-            return File.ReadAllText(path);
+            var fileEncoding = GetFileEncoding(path);
+
+            return File.ReadAllText(path, fileEncoding);
         }
 
         public void WriteContents(string path, string content)
         {
             this.MakeWritable(path);
+
+            var fileEncoding = GetFileEncoding(path);
 
             var hidden = this.IsHidden(path);
 
@@ -29,7 +33,7 @@ namespace Endjin.Templify.Domain.Domain.Packager.Processors
                 this.Unhide(path);
             }
 
-            File.WriteAllText(path, content, Encoding.UTF8);
+            File.WriteAllText(path, content, fileEncoding);
 
             if (hidden)
             {
@@ -55,6 +59,43 @@ namespace Endjin.Templify.Domain.Domain.Packager.Processors
         private void MakeWritable(string path)
         {
             File.SetAttributes(path, File.GetAttributes(path) & ~(FileAttributes.Archive | FileAttributes.ReadOnly));
+        }
+
+        /// <summary>
+        /// Detects the byte order mark of a file and returns
+        /// an appropriate encoding for the file.
+        /// </summary>
+        /// <param name="srcFile"></param>
+        /// <returns></returns>
+        private Encoding GetFileEncoding(string srcFile)
+        {
+            // *** Use Default of Encoding.Default (Ansi CodePage)
+            Encoding enc = Encoding.Default;
+
+            // *** Detect byte order mark if any - otherwise assume default
+            var buffer = new byte[5];
+            var file = new FileStream(srcFile, FileMode.Open);
+            file.Read(buffer, 0, 5);
+            file.Close();
+
+            if (buffer[0] == 0xef && buffer[1] == 0xbb && buffer[2] == 0xbf)
+            {
+                enc = Encoding.UTF8;
+            }
+            else if (buffer[0] == 0xfe && buffer[1] == 0xff)
+            {
+                enc = Encoding.Unicode;
+            }
+            else if (buffer[0] == 0 && buffer[1] == 0 && buffer[2] == 0xfe && buffer[3] == 0xff)
+            {
+                enc = Encoding.UTF32;
+            }
+            else if (buffer[0] == 0x2b && buffer[1] == 0x2f && buffer[2] == 0x76)
+            {
+                enc = Encoding.UTF7;
+            }
+
+            return enc;
         }
     }
 }
